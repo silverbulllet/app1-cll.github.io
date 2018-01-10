0793c833cdc35c42e300bd4b2f006e7b2e946275
Leo_0 <leo_0@Leo-0s-MacBook-Pro.local>
2018-01-10 14:26:39 +0800

    edit serviceWorker

--- sw.js
+++ sw.js
@@ -2,11 +2,23 @@
 
 console.log(' in sw.js ');
 
+var CACHE_NAME = 'my-site-cache-v1';
+var urlsToCache = [
+  '/',
+  '/styles/main.css',
+  '/script/main.js'
+];
 
 self.addEventListener('install', function(event) {
-	console.log('installing...', { event })
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
 });
-
 
 self.addEventListener('fetch', function(event) {
 	console.log('fetch...', { event })
